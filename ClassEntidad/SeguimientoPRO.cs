3044110be35ef504157ff1b95ca2870dfb4c44e0
Ian4Lopez <git241017hl@gmail.com>
2022-07-31 19:03:10 -0500

    Se juntaron los cambios

--- ClassEntidad/SeguimientoPRO.cs
+++ ClassEntidad/SeguimientoPRO.cs
@@ -16,5 +16,7 @@ namespace ClassEntidad
         public string Reporte { set; get; }
         public string Entrevista { set; get; }
         public string Extra { set; get; }
+        public string Imagen { set; get; }
+
     }
 }
